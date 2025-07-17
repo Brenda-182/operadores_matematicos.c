fb620f70e86db8e98aeeb086fa986a6fdd614e00
Brenda-182 <brendaassis2002@gmail.com>
2025-07-17 09:43:33 -0300

    primeira parte feita

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 int main(){
-    printf("hii github\n");
+    int a = 10;
+    int b = 5;
+
+    //Operadores aritméticos
+    int soma = a + b;
+    int subtração = a - b;
+    int multiplicação = a * b;
+    int divisão = a / b;
+
+    //Operadores de atribuição
+    a += 2; //a será 12
+    b *= 3; //b será 15
+
+    //Operadores de incremento e decremento
+    a ++; // a será 13
+    b --; //b sera 14
+
+    //Exibição dos resultados
+    printf("Soma: %d\n", soma);
+    printf("Subtração: %d\n", subtração);
+    printf("Multiplicação: %d\n", multiplicação);
+    printf("Dvisão: %d\n", divisão);
+    printf("Novo valor de a(após += 2 e ++): %d\n", a);
+    printf("Novo valor de b(após *= 3 e --): %d\n", b);
+
     return 0;
 }
